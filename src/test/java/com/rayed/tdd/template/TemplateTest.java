96083970524126e1f574d89baf54fd6f0f7a316c
Rayed Bin Wahed <rayed@therapservices.net>
2018-08-09 14:00:55 +0600

    Refactor tests

--- src/test/java/com/rayed/tdd/template/TemplateTest.java
+++ src/test/java/com/rayed/tdd/template/TemplateTest.java
@@ -1,6 +1,7 @@
 package com.rayed.tdd.template;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,41 +12,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class TemplateTest {
 
-    @Test
-    public void oneVariable() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Reader");
-        assertEquals("Hello, Reader", template.evaluate());
-    }
+    private Template template;
 
-    @Test
-    public void differentValue() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "someone else");
-        assertEquals("Hello, someone else", template.evaluate());
-    }
-
-    @Test
-    public void differentTemplate() throws Exception {
-        Template template = new Template("Hi, ${name}");
-        template.set("name", "Reader");
-        assertEquals("Hi, Reader", template.evaluate());
-    }
-
-    @Test
-    public void multipleVariables() throws Exception {
-        Template template = new Template("${one}, ${two}, ${three}");
+    @BeforeEach
+    public void setUp() {
+        template = new Template("${one}, ${two}, ${three}");
         template.set("one", "1");
         template.set("two", "2");
         template.set("three", "3");
-        assertEquals("1, 2, 3", template.evaluate());
     }
 
     @Test
-    public void unknownVariablesAreIgnored() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Reader");
-        template.set("doesnotexist", "Hi");
-        assertEquals("Hello, Reader", template.evaluate());
+    public void evaluateMultipleTemplateVariables() throws Exception {
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    @Test
+    public void ignoreUnusedTemplateVariables() throws Exception {
+        template.set("doesNotExist", "does not exist");
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    private void assertTemplateEvaluatesTo(String expected) {
+        assertEquals(expected, template.evaluate());
     }
 }
