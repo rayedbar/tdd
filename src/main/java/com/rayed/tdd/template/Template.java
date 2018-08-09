559b3d935ceed88ffde977a440651b76a433e9a3
Rayed Bin Wahed <rayed@therapservices.net>
2018-08-09 16:26:38 +0600

    Add test for missing values

--- src/main/java/com/rayed/tdd/template/Template.java
+++ src/main/java/com/rayed/tdd/template/Template.java
@@ -1,7 +1,11 @@
 package com.rayed.tdd.template;
 
+import com.rayed.tdd.exceptions.MissingValueException;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author rayed
@@ -14,7 +18,7 @@ public class Template {
 
     public Template(String templateText) {
         this.templateText = templateText;
-        this.variables = new HashMap<String, String>();
+        this.variables = new HashMap<>();
     }
 
     public void set(String variable, String value) {
@@ -22,11 +26,24 @@ public class Template {
     }
 
     public String evaluate() {
+        String result = substituteVariablesWithValues();
+        checkForMissingValues(result);
+        return result;
+    }
+
+    private String substituteVariablesWithValues() {
         String result = templateText;
         for (Map.Entry<String, String> entry : variables.entrySet()) {
             String regex = "\\$\\{" + entry.getKey() + "\\}";
             result = result.replaceAll(regex, entry.getValue());
         }
         return result;
+    }
+
+    private void checkForMissingValues(String result) {
+        Matcher matcher = Pattern.compile("\\$\\{.+\\}").matcher(result);
+        if (matcher.find()) {
+            throw new MissingValueException("No value for " + matcher.group());
+        }
     }
 }
--- src/test/java/com/rayed/tdd/template/TemplateTest.java
+++ src/test/java/com/rayed/tdd/template/TemplateTest.java
@@ -1,9 +1,11 @@
 package com.rayed.tdd.template;
 
+import com.rayed.tdd.exceptions.MissingValueException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @author rayed
@@ -30,6 +32,16 @@ public class TemplateTest {
     public void ignoreUnusedTemplateVariables() throws Exception {
         template.set("doesNotExist", "does not exist");
         assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    @Test
+    public void missingValueRaisesException() throws Exception {
+        try {
+            new Template("${foo}").evaluate();
+            fail("evaluate() should throw a MissingValueException if a template variable does not have a value");
+        } catch (MissingValueException expected) {
+            assertEquals("No value for ${foo}", expected.getMessage());
+        }
     }
 
     private void assertTemplateEvaluatesTo(String expected) {
