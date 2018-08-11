2658524d6d6336876b9aec0eeabb8d574ce8efd9
Rayed Bin Wahed <rayed@therapservices.net>
2018-08-11 17:10:30 +0600

    Add test to measure rendering performance

--- src/test/java/com/rayed/tdd/template/TemplatePerformanceTest.java
+++ src/test/java/com/rayed/tdd/template/TemplatePerformanceTest.java
@@ -1,9 +1,52 @@
 package com.rayed.tdd.template;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * @author rayed
  * @since August 11, 2018
  */
 public class TemplatePerformanceTest {
+
+    private Template template;
+
+    @BeforeEach
+    public void setUp() {
+        buildTemplate();
+        populateTemplate();
+    }
+
+    private void buildTemplate() {
+        StringBuffer text = new StringBuffer(10000);
+        for (int i = 0, var = 1; i < 100; i++) {
+            text.append(" template ");
+            if (i % 5 == 0) {
+                text.append("${var").append(var).append("}");
+                var = var + 1;
+            }
+        }
+        template = new Template(text.toString());
+    }
+
+    private void populateTemplate() {
+        for (int var = 1; var < 21; var++) {
+            template.set("var" + var, "value of var" + var);
+        }
+    }
+
+    @Test
+    public void templateWith1000WordsAnd20Variables() {
+        long expected = 200L;
+        long time = System.currentTimeMillis();
+        String result = template.evaluate();
+        time = System.currentTimeMillis() - time;
+        assertTrue(time <= expected, "Rendering the template took " + time + "ms while the target was " +
+                expected + "ms");
+    }
+
 
 }
